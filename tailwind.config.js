b6d4c613498beb949abf3b67bcd44252db8a7202
zitsav <utsav19october@gmail.com>
2023-09-26 23:05:05 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/html/utils/withMT");
+module.exports = withMT ({
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "emerald",
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+});
+
